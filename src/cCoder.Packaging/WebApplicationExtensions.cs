c385adbf86f58e9828524eed8bb0ef7c31629ce6
TehWardy <paul.ward@ccoder.co.uk>
2026-04-10 14:29:57 +0100

    Remove content metadata cache coupling

--- src/cCoder.Packaging/WebApplicationExtensions.cs
+++ src/cCoder.Packaging/WebApplicationExtensions.cs
@@ -9,8 +9,6 @@ namespace cCoder.Packaging;
 public static class WebApplicationExtensions
 {
     private const string MetadataScope = "Packaging";
-    private const string ContentMetadataCacheTypeName =
-        "cCoder.ContentManagement.Exposures.Caching.IMetadataCache, cCoder.ContentManagement";
 
     public static WebApplication UsePackagingExposure(this WebApplication app, ILogger log = null)
     {
@@ -32,14 +30,5 @@ public static class WebApplicationExtensions
                     .GetKnownMetadata()
                     .Select(static metadata => JsonSerializer.Serialize(metadata)));
         }
-
-        RebuildContentMetadataCache(app.Services);
-    }
-
-    private static void RebuildContentMetadataCache(IServiceProvider services)
-    {
-        Type metadataCacheType = Type.GetType(ContentMetadataCacheTypeName, throwOnError: false);
-        object metadataCache = metadataCacheType is null ? null : services.GetService(metadataCacheType);
-        _ = metadataCacheType?.GetMethod("Rebuild")?.Invoke(metadataCache, null);
     }
 }
